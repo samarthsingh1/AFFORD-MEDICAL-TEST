7e3ac0b2e9e0454ccf25a1c5229403f1e009a58c
samarthsingh1 <48879672+samarthsingh1@users.noreply.github.com>
2020-09-12 09:30:43 +0530

    Add files via upload

---
+++ Problem2.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class Practice {
+	public boolean checkValidity(char[][] board,String str) {
+		if(board==null||str==null) {
+			return true;
+		}
+		for(int i=0;i<board.length;i++) {
+			for(int j=0;j<board[i].length;i++) {
+				if(board[i][j]==str.charAt(0)&& dfs(board,i,j,0,str)) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+	public boolean dfs(char[][] board,int i1,int i2,int i3,String str) {
+		if(i3==str.length()) {
+			return true;
+		}
+		if(i1<0||i1>=board.length||i2<0||i2>board[i1].length||board[i1][i2]!=str.charAt(i3)) {
+			return false;
+		}
+		char temp=board[i1][i2];
+		board[i1][i2]=' ';
+		boolean located=dfs(board,i1+1,i2,i3+1,str)||dfs(board,i1-1,i2,i3+1,str)||dfs(board,i1,i2+1,i3+1,str)||dfs(board,i1,i2-1,i3+1,str);
+		board[i1][i2]=temp;
+		return located;
+	}
+	
+	
+
+}
