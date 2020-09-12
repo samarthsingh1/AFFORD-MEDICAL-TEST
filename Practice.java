ac8fa6470319e380461f6f14bd09891be88b7ad2
samarthsingh1 <48879672+samarthsingh1@users.noreply.github.com>
2020-09-12 08:38:05 +0530

    Update Practice.java

--- Practice.java
+++ Practice.java
@@ -1,3 +1,10 @@
+/*Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input
+string is valid. An input string is valid if –
+ Open brackets must be closed by the same type of brackets.
+ Open brackets must be closed in the correct order.
+	*/
+
+
 import java.util.*;
 public class Practice {
 	private HashMap<Character,Character> map;
